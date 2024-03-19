bb58a37f74da7ec1fc46e551f513ac24ecc61acf
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-03-19 18:09:33 +0100

    fix style

--- argentbank/src/pages/connectpage/Connectpage.jsx
+++ argentbank/src/pages/connectpage/Connectpage.jsx
@@ -6,21 +6,21 @@ export function ConnectPage() {
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon" />
-        <h1>Se connecter</h1>
+        <h1>Sign In </h1>
         <form>
           <div className="input-wrapper">
-            <label htmlFor="username">Nom d'utilisateur</label>
+            <label htmlFor="username">Username</label>
             <input type="text" id="username" />
           </div>
           <div className="input-wrapper">
-            <label htmlFor="password">Mot de passe</label>
+            <label htmlFor="password">Password</label>
             <input type="password" id="password" />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
-            <label htmlFor="remember-me">Se souvenir de moi</label>
+            <label htmlFor="remember-me">Remember me</label>
           </div>
-          <button className="sign-in-button">Se connecter</button>
+          <button className="sign-in-button">Sign In</button>
         </form>
       </section>
     </main>
