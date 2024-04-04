be13c25d939bd8d4fc144de472519ae882bf9633
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2024-04-04 04:04:46 +0200

    tentative de fix userName

--- argentbank/src/components/header/header.jsx
+++ argentbank/src/components/header/header.jsx
@@ -1,39 +1,3 @@
-// import React from 'react';
-// import './header.scss';
-// import argentBankLogo from '../../assets/img/argentBankLogo.png';
-// import { Link } from 'react-router-dom';
-// import { useDispatch,useSelector } from 'react-redux';
-
-
-// export function Header() {
-//   const token = useSelector ((state)=>state.auth.token)
-//     return (
-//       <div>
-//         <title>Argent Bank - Home Page</title>
-//         <nav className="main-nav">
-//           <a className="main-nav-logo" href="./index.html">
-//             <Link to="/" >
-//             <img className="main-nav-logo-image" src={argentBankLogo} alt="Argent Bank Logo" />
-//             </Link>
-//             <h1 className="sr-only">Argent Bank</h1>
-//           </a>
-//           <div>
-//             {token?(
-//               <>
-//               <p>je suis connecté</p>
-//               </>
-//             ):(
-//               <Link className="main-nav-item" to="/connect-page">
-//               <i className="fa fa-user-circle" />
-//               Sign In
-//             </Link>
-//             )}
-           
-//           </div>
-//         </nav>
-//       </div>
-//     );
-//   }
 import React from 'react';
 import './header.scss';
 import argentBankLogo from '../../assets/img/argentBankLogo.png';
@@ -45,7 +9,7 @@ import { logout } from '../../redux-toolkit/reducers/authSlice';
 
 export function Header() {
   const token = useSelector((state) => state.auth.token);
-  const username = useSelector((state) => state.auth.username);
+  const userName = useSelector((state) => state.auth.userName);
   const [email, setEmail] = useState('');
   const dispatch= useDispatch()
 
@@ -68,7 +32,7 @@ export function Header() {
           {token ? (
             <>
               <p>
-              {username} <span className="main-nav-username"></span>
+              {userName} <span className="main-nav-username"></span>
               </p>
               <Link onClick={()=>{
                 dispatch(logout())
